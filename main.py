2dd91f5088c6651966e559625ce0f45519df604b
Ageng Anugrah Wardoyo Putra <agenganugrah@gmail.com>
2024-07-17 15:01:45 +0700

    Add gor sima

--- main.py
+++ main.py
@@ -12,13 +12,40 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+def merge_intervals(intervals):
+  intervals = [tuple(interval.split(' - ')) for interval in intervals]
+  
+  intervals.sort(key=lambda x: x[0])
+  
+  merged_intervals = []
+  current_start, current_end = intervals[0]
 
-def check_jadwal(date):
+  for start, end in intervals[1:]:
+    if start <= current_end:
+      current_end = max(current_end, end)
+    else:
+      merged_intervals.append((current_start, current_end))
+      current_start, current_end = start, end
+
+  merged_intervals.append((current_start, current_end))
+
+  merged_time_intervals = [
+    f"{start} - {end}"
+    for start, end in merged_intervals
+  ]
+
+  return merged_time_intervals
+
+def check_jadwal(place, date):
   empty_slots = set()
-  payload = {"venue_id": 307, "date": date}
+  vanue_code = {
+      "kukusan": 307,
+      "sima": 726
+  }
+  payload = {"venue_id": vanue_code[place.lower()], "date": date}
 
   response = requests.get(
-      f"https://ayo.co.id/venues-ajax/op-times-and-fields?venue_id=307&date={date}",
+      f"https://ayo.co.id/venues-ajax/op-times-and-fields",
       params=payload).json()
 
   if not len(response["op_time"]["hours"]):
@@ -34,9 +61,10 @@ def check_jadwal(date):
 
   empty_slots = list(empty_slots)
   empty_slots.sort()
-  joined_slots = '\n'.join(empty_slots)
+  merges_slots = merge_intervals(empty_slots)
+  joined_slots = '\n'.join(merges_slots)
   text = f"""
-Berikut jadwal GOR Kukusan yang tersedia untuk Hari {response['op_time']['day']}, Tanggal {date}:
+Berikut jadwal GOR {place.capitalize()} yang tersedia untuk Hari {response['op_time']['day']}, Tanggal {date}:
 
 {joined_slots}
   """
@@ -66,11 +94,11 @@ async def on_message(message):
 @bot.tree.command(name='jadwal')
 @app_commands.describe(place='Nama GOR', date='YYYY-MM-DD')
 async def jadwal(interaction, place: str, date: str):
-  if place.lower() != 'kukusan':
+  if not(place.lower() in ['kukusan', 'sima']):
     await interaction.response.send_message(
-        "Maaf, baru bisa gor kukusan Kak 😭😭")
+        "Maaf, baru bisa gor kukusan dan sima Kak 😭😭")
     return
-  res = check_jadwal(date)
+  res = check_jadwal(place, date)
   await interaction.response.send_message(res)
 
 
